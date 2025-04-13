9e145ac80775bb1f4e4bc02dcd33f1921c85eb7b
Thomas Bell <thomasjbell065@gmail.com>
2025-04-13 17:37:39 -0400

    gif change
    changed giphy image

--- new-class-template.sh
+++ new-class-template.sh
@@ -56,7 +56,7 @@ cat <<EOF > /var/www/html/index.html
   <div class="bgimg w3-display-container w3-animate-opacity w3-text-white">
     <div class="w3-display-topleft w3-padding-large w3-xlarge"></div>
     <div class="w3-display-middle w3-center">
-      <iframe src="https://giphy.com/embed/1sv8rIfAtyMsqsqDt8"
+      <iframe src="https://giphy.com/embed/Kz7PNyq1r5TSNH37JM"
               width="500"
               height="270"
               style="border-radius:10px;"
